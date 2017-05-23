4919ad0c5ea7ac94954d9ae305eff6e0f69060aa
Juan Ignacio <juanignaciomln@gmail.com>
2017-05-23 19:12:59 -0300

    Added DelayedTextWatcher to the search view

--- app/src/main/java/ar/com/wolox/kotlintest/screens/search/SearchView.kt
+++ app/src/main/java/ar/com/wolox/kotlintest/screens/search/SearchView.kt
@@ -1,8 +1,14 @@
 package ar.com.wolox.kotlintest.screens.search
 
 import android.content.Context
+import android.text.InputType
 import android.widget.LinearLayout
+import ar.com.wolox.kotlintest.R
+import ar.com.wolox.kotlintest.components.DelayedTextWatcher
+import ar.com.wolox.kotlintest.components.GifRecyclerComponent
+import com.github.ybq.android.spinkit.style.DoubleBounce
 import com.wealthfront.magellan.BaseScreenView
+import trikita.anvil.Anvil
 import trikita.anvil.DSL.*
 import trikita.anvil.RenderableView
 
@@ -15,21 +21,52 @@ class SearchView(context: Context) : BaseScreenView<SearchScreen>(context) {
                     size(MATCH, MATCH)
                     orientation(LinearLayout.VERTICAL)
 
-                    textView{
-                        size(MATCH, 0)
-                        weight(1f)
-                        text("Detail View")
-                        textSize(sip(32f))
+                    linearLayout {
+                        size(MATCH, dip(96))
+                        orientation(LinearLayout.HORIZONTAL)
+                        padding(dip(16), dip(32), dip(16), dip(8))
+                        backgroundColor(ar.com.wolox.kotlintest.R.color.colorPrimary)
+
+                        imageView {
+                            size(dip(24), dip(24))
+                            imageResource(R.drawable.ic_search)
+                            layoutGravity(BOTTOM)
+                            margin(0, 0, 0, dip(16))
+                        }
+
+                        editText {
+                            size(0, MATCH)
+                            weight(1f)
+                            hint("Search something...")
+                            textSize(sip(18f))
+                            singleLine(true)
+                            margin(dip(8), 0, 0, 0)
+                            inputType(InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS)
+
+                            onTextChanged(DelayedTextWatcher({
+                                query ->
+                                screen.presenter.searchGifs(query)
+                            }))
+
+                        }
+
+                        progressBar {
+                            size(dip(24), dip(24))
+                            visibility(screen.store.state.isFetching)
+                            layoutGravity(BOTTOM)
+                            margin(0, 0, 0, dip(16))
+                            indeterminateDrawable(DoubleBounce())
+                        }
                     }
 
-                    button{
-                        size(WRAP, WRAP)
-                        text("Go back")
-                        onClick { screen.navigator.goBack() }
-                    }
+                    GifRecyclerComponent(context, screen.store.state.gifs)
                 }
             }
 
+            // TODO TEMPORAL FIX FOR RECYCLER ISSUE
+            override fun onDetachedFromWindow() {
+                Anvil.unmount(this, false)
+            }
         })
     }
 
